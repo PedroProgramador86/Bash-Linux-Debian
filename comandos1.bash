317431445dde7541f1dbe84dcc319340e7fee325
Bazuka <bazukaffpedro@gmail.com>
2024-11-02 12:06:43 -0300

    Comitando novas alterações

--- comandos1.bash
+++ comandos1.bash
@@ -7,11 +7,13 @@ cd diretorioquedesejaentrar
 #Mostra o caminho de onde você se encontra
 pwd 
 
-touch nomedapasta.txt
+#Cria um Arquivo vazio
+
+touch nomedoarquivo.txt
 
 #Utiliza editores de texto para editar um arquivo
 
-nano nomedapasta.txt
+nano nomedoarquivo.txt
 
 #Exibe o conteudo dentro de um arquivo
-cat nomedapasta.txt
+cat nomedoarquivo.txt
