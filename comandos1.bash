afa37f1ad0fe5bf7f18af1c7aec9f3dc967c7e66
Bazuka <bazukaffpedro@gmail.com>
2024-11-02 15:09:21 -0300

    salvando comando de voltar diretorio

--- comandos1.bash
+++ comandos1.bash
@@ -4,6 +4,9 @@ ls
 #Entra em um diretorio
 cd diretorioquedesejaentrar
 
+#Volta para o diretorio anterior
+cd ..
+
 #Mostra o caminho de onde você se encontra
 pwd 
 
@@ -14,6 +17,8 @@ touch nomedoarquivo.txt
 #Utiliza editores de texto para editar um arquivo
 
 nano nomedoarquivo.txt
+
+#(OBS: Para sair do editor 'nano' se pressiona as teclas 'ctrl + x' e apeta em 'y' para salvar ou 'n' para não salvar)
 
 #Exibe o conteudo dentro de um arquivo
 cat nomedoarquivo.txt
