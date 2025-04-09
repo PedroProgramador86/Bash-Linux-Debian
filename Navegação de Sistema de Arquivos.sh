d82a0c12303a50793800791039e1aa3ba772fb4c
Pedro Enzo <bazukastream8866@gmail.com>
2025-04-09 17:49:01 -0300

    Navegação no Sistema de Arquivos

---
+++ Navegação de Sistema de Arquivos.sh
@@ -0,0 +1,15 @@
+# Mostra o local do diretorio atual
+# "pwd" - "print working directory"
+
+pwd
+
+# Lista Arquivos e diretorios
+# "ls" - list
+
+ls [opções] [caminho/do/diretório]
+
+# Mudar de diretorio
+# "cd" Change Directory
+
+cd [caminho/do/diretório ou nome/do/diretorio/]
+
