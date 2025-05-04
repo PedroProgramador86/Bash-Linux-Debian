742ff38f9852887f7a476781e42806fed77f0753
Pedro Enzo <bazukastream8866@gmail.com>
2025-05-04 19:23:47 -0300

    Finalizando anotações de 'Navegação de Sistemas de Arquivos'

---
+++ Comando Essenciais/Navegação de Sistema de Arquivos.sh
@@ -0,0 +1,31 @@
+# Mostra o local do diretorio atual
+# "pwd" - Mostra o diretorio atual
+
+pwd
+
+# Lista Arquivos e diretorios
+# "ls" - lista arquivos e diretorios
+
+ls [opções] [caminho/do/diretório]
+
+# [opções]:
+# -l Lista detalhada
+# -a Mostra todos os arquivos (incluindo ocultos)
+# -h Utiliza leitura de tamanhos legiveis (KB, MB, GB) combando com o -l
+# -R Lista os arquivos de forma recursiva (diretorios e subdiretorios)
+# -S Ordena o tamanho dos arquivos (Do maior para o menor)
+# -t Ordena por data de modificação (sendo o primeiro o mais recente)
+# -r Inverte a ordem de listagem (usado com -S, -t, etc.)
+# -d Mostra apenas o próprio diretório, sem listar seu conteúdo (ex: ls -d */)
+# -1 Exibe um arquivo por linha (sendo util para scripts)
+
+# Mudar de diretorio
+# "cd" Change Directory
+
+cd [caminho/do/diretório ou nome/do/diretorio/]
+
+#[Caminhos mais comuns]:
+# .. volta um nivel
+# [Apenas o cd] volta para home (~)
+# / vai para o diretorio raiz
+
